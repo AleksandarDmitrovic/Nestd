c9b1984095f632d5e563d625c28650208628dd86
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2025-11-05 12:01:02 -0700

    add template dashboard component

---
+++ src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,13 @@
+import PropTypes from "prop-types";
+
+const Dashboard = (props) => {
+  return (
+    <>
+      <header></header>
+    </>
+  );
+};
+
+Dashboard.propTypes = {};
+
+export default Dashboard;
