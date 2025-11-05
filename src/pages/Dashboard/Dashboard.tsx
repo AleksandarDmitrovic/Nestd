ca6decc43c4f7b91e9cc5e494785942e9bc21d57
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2025-11-05 12:47:50 -0700

    add menu icon

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,10 +1,12 @@
 import styles from "./Dashboard.module.css";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Dashboard = () => {
   return (
     <>
       <header className={styles.header}>
         <h1 className={styles.title}>Nestd</h1>
+        <MenuIcon fontSize="large" />
       </header>
     </>
   );
