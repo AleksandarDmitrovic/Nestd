607e2fd57f35981d484b385a335518a72cebd356
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2025-11-05 13:13:58 -0700

    create total retirement section

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -8,6 +8,15 @@ const Dashboard = () => {
         <h1 className={styles.title}>Nestd</h1>
         <MenuIcon fontSize="large" />
       </header>
+      <main>
+        <div className={styles.total_container}>
+          <span className={styles.total_title}>Total Retirement Savings</span>
+          <span className={styles.total_value}>$487,320</span>
+          <span className={styles.total_subtitle}>
+            Combined family accounts
+          </span>
+        </div>
+      </main>
     </>
   );
 };
