d9b8a356f7e4d822090e2eeced1e3d6f535f024a
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2025-12-11 18:38:00 -0700

    add cors dep to server

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,11 @@
 import express from "express";
+import cors from "cors";
+
 const app = express();
+
+const corsOptions = { origin: ["http://localhost:5173"] };
+
+app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
