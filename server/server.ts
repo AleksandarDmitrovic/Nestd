961e625e32c2564b0997734b864fc842bd8ceb77
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2025-12-14 10:57:09 -0700

    add snaptrade sdk & login api call to server

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,20 @@
 import express from "express";
 import cors from "cors";
+import { Snaptrade } from "snaptrade-typescript-sdk";
+import { loadEnvFile } from "node:process";
+loadEnvFile("./.env");
 
 const app = express();
 
 const corsOptions = { origin: ["http://localhost:5173"] };
 
 app.use(cors(corsOptions));
+app.use(express.json());
+
+const snaptrade = new Snaptrade({
+  clientId: process.env.SNAPTRADE_CLIENT_ID,
+  consumerKey: process.env.SNAPTRADE_SECRET,
+});
 
 app.get("/api", (req, res) => {
   // res.send("Hello, World!");
@@ -16,6 +25,22 @@ app.get("/api", (req, res) => {
       "Investment Account 3",
     ],
   });
+});
+
+app.post("/api/snaptrade/login", async (req, res) => {
+  try {
+    const { userId } = req.body;
+    const userSecret = ""; // Retrieve from env for testing refactor for mongo db later
+
+    const result = await snaptrade.authentication.loginSnapTradeUser({
+      userId: process.env.SNAPTRADE_USER_ID || userId,
+      userSecret: process.env.SNAPTRADE_USER_SECRET || userSecret,
+    });
+
+    res.json(result.data);
+  } catch (error: unknown) {
+    res.status(500).json({ error, message: "Failed to login to SnapTrade" });
+  }
 });
 
 app.listen(8080, () => {
