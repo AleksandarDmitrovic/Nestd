24e68dde237f5ade347cf7d808603ecab678ba9c
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2025-12-16 20:35:31 -0700

    update add user function calling add user api endpoint

--- client/src/api/users.ts
+++ client/src/api/users.ts
@@ -1,12 +1,12 @@
 export const addUser = async () => {
   try {
-    const response = await fetch("http://localhost:8080/api/users", {
+    const response = await fetch("http://localhost:8080/api/addUser", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        email: "aleksandar.dmitrovic@gmail.com",
-        firstName: "Aleksandar",
-        lastName: "Dmitrovic",
+        email: import.meta.env.VITE_ADD_USER_EMAIL,
+        firstName: import.meta.env.VITE_ADD_USER_FIRST_NAME,
+        lastName: import.meta.env.VITE_ADD_USER_LAST_NAME,
       }),
     });
 
